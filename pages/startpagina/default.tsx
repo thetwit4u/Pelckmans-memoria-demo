7e4b70b6948fe3ee5af2731fad19f8e5456683c1
Sebastian Vuye <sebastian.vuye@gmail.com>
2021-12-15 10:14:20 +0100

    feat: add navigation bar placeholder

--- pages/startpagina/default.tsx
+++ pages/startpagina/default.tsx
@@ -11,7 +11,15 @@ const Default: NextPage = () => {
       </Head>
 
       <main className='grid grid-cols-3 mx-auto'>
-        <div className='fixed left-0 w-4/12 h-screen bg-secondary' />
+        {/* navigation bar */}
+        <div className='fixed left-0 w-4/12 h-screen bg-secondary'>
+          <section className='bg-primary'>
+            <div className='flex justify-between mx-auto w-11/12'>
+              <h1 className='text-4xl'>Memoria</h1>
+              <span className='text-4xl'>X</span>
+            </div>
+          </section>
+        </div>
         <div className='col-span-2 col-start-2 bg-primary'>
           {/* article content */}
           <div className='grid grid-cols-2 mx-auto w-10/12 bg-primary-light'>
