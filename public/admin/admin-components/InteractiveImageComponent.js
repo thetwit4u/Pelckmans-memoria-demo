4b4976a5ff20a0068668f8158a70383c1e72ba73
Peeters, Stefano <stefano.peeters@wolterskluwer.com>
2022-02-03 17:54:44 +0100

    feat: wip interactive images

--- public/admin/admin-components/InteractiveImageComponent.js
+++ public/admin/admin-components/InteractiveImageComponent.js
@@ -1,22 +1,85 @@
 const Control = createClass({
-  handleChange: function (e) {
-    this.props.onChange(e.target.value.trim());
-  },
-
-  render: function () {
-    const value = this.props.value;
-    return h('input', {
-      id: this.props.forID,
-      className: this.props.classNameWrapper,
-      type: 'text',
-      value: value,
-      onChange: this.handleChange
+  valueCache: new Map(),
+  imageUploaded: false,
+  imageUrl: '',
+  pointsLength: 0,
+  blobToImage: blob => {
+    return new Promise(resolve => {
+      const url = URL.createObjectURL(blob);
+      let img = new Image();
+      img.onload = () => {
+        URL.revokeObjectURL(url);
+        resolve(img);
+      };
+      img.src = url;
     });
+  },
+  mergeInputValues(e, prefix, key, self) {
+    self.valueCache.set(prefix, { ...self.valueCache.get(prefix), ...{ [key]: e.target.value } });
+    self.props.onChange(self.valueCache);
+  },
+  addPoint() {
+    this.pointsLength++;
+    this.forceUpdate();
+  },
+  removePoint() {
+    this.pointsLength--;
+    this.forceUpdate();
+  },
+  imageInputHandler(evt) {
+    const [file] = evt.target.files;
+    if (file) {
+      this.imageUrl = URL.createObjectURL(file);
+      this.imageUploaded = true;
+    } else {
+      this.imageUploaded = false;
+    }
+    this.forceUpdate();
+  },
+  removeSelectedImage() {
+    console.log('remove selected');
+    this.imageUrl = '';
+    this.imageUploaded = false;
+    this.forceUpdate();
+  },
+  renderImage() {
+    const args = {
+      src: this.imageUrl
+    };
+    return h('div', {}, [
+      h('img', args),
+      h('div', {}, [
+        h('span', {}, `Selected image: ${this.imageUrl}`),
+        h(
+          'button',
+          {
+            onClick: () => {
+              this.removeSelectedImage();
+            }
+          },
+          `Remove image`
+        )
+      ])
+    ]);
+  },
+  renderFileInput() {
+    const args = {
+      type: 'file',
+      id: 'file-input--interactiveComponent',
+      accept: 'image/png, image/jpeg, image/png, image/jpg',
+      onInput: e => this.imageInputHandler(e)
+    };
+    return h('input', args);
+  },
+  render: function () {
+    return h('div', null, [this.imageUploaded ? this.renderImage() : this.renderFileInput()]);
   }
 });
 
 const Preview = createClass({
+  mounted: false,
   imgPreview: {
+    imgUrl: '',
     parentDivElement: HTMLElement,
     element: HTMLElement,
     dimensions: {
@@ -24,14 +87,11 @@ const Preview = createClass({
       height: 0
     }
   },
-  randomPoint: {
-    x: 100,
-    y: 100
-  },
   componentDidMount() {
     this.setElement();
     this.setDimensions();
-    this.addPoints();
+    this.addPointsToPreview();
+    this.mounted = true;
   },
   setPointStyles(point) {
     point.style.position = 'absolute';
@@ -48,30 +108,44 @@ const Preview = createClass({
   setElement() {
     const previewPane = document.querySelector('#preview-pane');
     this.imgPreview.element = previewPane.contentWindow.document.querySelector('img');
+    if (this.imgPreview && this.imgPreview.element) {
+    }
     this.imgPreview.parentDivElement = this.imgPreview.element ? this.imgPreview.element.parentNode : null;
-    console.log(this.imgPreview);
   },
   setDimensions() {
     const { element } = this.imgPreview;
     this.imgPreview.dimensions.width = element.width;
     this.imgPreview.dimensions.height = element.height;
   },
-  addPoints() {
+  addPointsToPreview() {
+    console.log(this.props.value);
     // get image parent div
     const imageParentDiv = this.imgPreview.element.parentElement;
     // set position relative on image parent div
     imageParentDiv.style.position = 'relative';
-    // create span points
-    const point = document.createElement('span');
-    // set span styling absolute
-    this.setPointStyles(point);
-    this.setPointPosition(point, { x: this.randomPoint.x, y: this.randomPoint.y });
-    // add span elements with this.randomPoint properties
-    imageParentDiv.append(point);
+
+    // if(!!this.props.value && Array.isArray(this.props.value)){
+    //   this.props.value.map()
+    //   // create span points
+    //   const point = document.createElement('span');
+    //   // set span styling absolute
+    //   this.setPointStyles(point);
+    //   this.setPointPosition(point, { x: this.randomPoint.x, y: this.randomPoint.y });
+    //   // add span elements with this.randomPoint properties
+    //   imageParentDiv.append(point);
+    // }
+  },
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
   },
   render: function () {
-    const { fieldsMetaData, value } = this.props;
-    return h('p', {}, [JSON.stringify(fieldsMetaData), value]);
+    // const { value } = this.props;
+    console.log('rerender preivew');
+    if (this.mounted) {
+      this.addPointsToPreview();
+    }
+
+    return h('p', {});
   }
 });
 
